d5e1c89e9498067149ad15a190d15893e4533534
Caique Costa <ccostafrias@gmail.com>
2023-04-20 19:47:02 -0300

    finish node-ejs

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -1,16 +1,41 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs');
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+    const qualities = [
+        {
+            title: 'D',
+            message: 'Duro na queda'
+        },
+        {
+            title: 'E',
+            message: 'EJS'
+        },
+        {
+            title: 'M',
+            message: 'Muito foda'
+        },
+        {
+            title: 'A',
+            message: 'Alegre'
+        },
+        {
+            title: 'I',
+            message: 'Imperfeito'
+        },
+        {
+            title: 'S',
+            message: 'Sem igual'
+        },
+    ]
+    res.render("pages/index", {
+        qualities
+    })
 })
+app.get("/sobre", (req, res) => res.render("pages/about"))
 
-app.get('/sonre', (req, res) => {
-    res.render('about')
-})
-
-app.listen(8080)
-
+app.listen(8888)
 console.log('Rodando')
